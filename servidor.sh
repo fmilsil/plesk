6313e3ed6ef41416c27414d84bc1dfb6fbca5dd5
fran <francisco@gmail.com>
2025-03-09 18:02:10 +0000

    despliegue del servidor

---
+++ servidor.sh
@@ -0,0 +1,8 @@
+# Descargamos las dependecias de plesk
+wget https://autoinstall.plesk.com/plesk-installer
+
+# Le damos permisos de ejecución 
+chmod +x plesk-installer
+
+# Ejecutamos el instalador
+./plesk-installer install plesk
